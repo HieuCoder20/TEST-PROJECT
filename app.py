0d7d9d490f4b829a37b4c385f34d45305d2da28c
HieuCoder20 <hieuoc2006@gmail.com>
2026-02-27 22:14:23 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,17 @@
-import eventlet
-eventlet.monkey_patch()
+from gevent import monkey
+monkey.patch_all() # Phải là dòng code thực thi đầu tiên
 
 import os
 from dotenv import load_dotenv
 load_dotenv()
 
+# Giờ mới import các thành phần của app
 from core import create_app, socketio
 
 app = create_app()
 
 if __name__ == "__main__":
+    # Lưu ý: socketio.run chỉ dùng cho môi trường local (development)
     port = int(os.environ.get("PORT", 5000))
-    socketio.run(app, host="0.0.0.0", port=port, debug=(os.getenv("FLASK_ENV") == "development"))
+    is_dev = os.getenv("FLASK_ENV") == "development"
+    socketio.run(app, host="0.0.0.0", port=port, debug=is_dev)
