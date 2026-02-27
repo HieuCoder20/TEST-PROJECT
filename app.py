f6130eafb0db9c9e013223c6537e88505307a89b
HieuCoder20 <hieuoc2006@gmail.com>
2026-02-27 22:32:13 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,3 @@
-from gevent import monkey
-monkey.patch_all()
-
 import os
 from dotenv import load_dotenv
 load_dotenv()
@@ -10,7 +7,8 @@ from core import create_app, socketio
 app = create_app()
 
 if __name__ == "__main__":
+    # Chỉ dùng monkey patch khi chạy local bằng lệnh: python app.py
+    from gevent import monkey
+    monkey.patch_all()
     port = int(os.environ.get("PORT", 5000))
-    # Chế độ debug chỉ bật khi chạy local
-    is_dev = os.getenv("FLASK_ENV") == "development"
-    socketio.run(app, host="0.0.0.0", port=port, debug=is_dev)
+    socketio.run(app, host="0.0.0.0", port=port, debug=True)
