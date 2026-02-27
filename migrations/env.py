bef7db57f0f8cc064c6cf5aeddfdf65fea60b70c
HieuCoder20 <hieuoc2006@gmail.com>
2026-02-27 22:34:02 +0700

    Update env.py

--- migrations/env.py
+++ migrations/env.py
@@ -1,6 +1,8 @@
 from gevent import monkey
+monkey.patch_all(thread=False, socket=False) 
+import gevent
+gevent.sleep(0)
 monkey.patch_all()
-
 import logging
 from logging.config import fileConfig
 
